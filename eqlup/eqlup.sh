09f5c83a38413606ed16a0336ac6d382993a6e88
Ian Guimaraes <ianguimaraes31@gmail.com>
2024-07-04 15:09:37 -0300

    feat: installation scripts

---
+++ eqlup/eqlup.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+USERNAME="iankressin"
+REPO_NAME="eql"
+REPO_URL="https://github.com/{$USERNAME}/${REPO_NAME}"
+REPO_API_URL="https://api.github.com/repos/${USERNAME}/${REPO_NAME}"
+
+LINUX_ASSET="x86_64-unknown-linux-musl.zip"
+MAC_ASSET="x86_64-apple-darwin.zip"
+
+get_latest_release_tag() {
+    LATEST_RELEASE_TAG=$(curl -s "${REPO_API_URL}/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+}
+
+initial_message() {
+    echo "
+    ███████╗ ██████╗ ██╗     ██╗   ██╗██████╗ 
+    ██╔════╝██╔═══██╗██║     ██║   ██║██╔══██╗
+    █████╗  ██║   ██║██║     ██║   ██║██████╔╝
+    ██╔══╝  ██║▄▄ ██║██║     ██║   ██║██╔═══╝ 
+    ███████╗╚██████╔╝███████╗╚██████╔╝██║     
+    ╚══════╝ ╚══▀▀═╝ ╚══════╝ ╚═════╝ ╚═╝
+
+        ((( The eql version manager )))
+    "
+
+    echo "[INFO] Installing the lastest version of eql: $LATEST_RELEASE_TAG"
+}
+
+detect_os() {
+    if [ "$OSTYPE" == "linux-gnu" ]; then
+        ASSET_NAME=$LINUX_ASSET
+        echo "[INFO] Linux detected"
+    elif [ "$OSTYPE" == "darwin"* ]; then
+        ASSET_NAME=$MAC_ASSET
+        echo "[INFO] MacOS detected"
+    elif [ "$OSTYPE" == "cygwin" ]; then
+        echo "[INFO] On Windows, download the executable from the link below:"
+        echo "{ $REPO_URL }/releases/latest"
+        exit 1
+    else
+        echo "[INFO] Unsupported OS"
+        exit 1
+    fi
+}
+
+download_asset() {
+    echo "[INFO] Downloading asset"
+    curl -L -o latest.zip "${REPO_URL}/releases/download/${LATEST_RELEASE_TAG}/${ASSET_NAME}"
+    echo "[INFO] Asset downloaded"
+}
+
+extract_asset() {
+    echo "[INFO] Extracting assets"
+    unzip latest.zip -d latest > /dev/null
+
+    if [ $? -ne 0 ]; then
+        echo "[INFO] Failed to unzip asset"
+        exit 1
+    fi
+    echo "[INFO] Assets extracted"
+}
+ 
+move_to_bin() {
+    echo "[INFO] Moving to /usr/local/bin"
+    sudo mv latest/eql /usr/local/bin/eql
+    chmod +x /usr/local/bin/eql
+    echo "[INFO] Installed to /usr/local/bin/eql"
+}
+
+cleanup() {
+    rm -rf latest latest.zip
+    echo "[INFO] Cleaned up"
+}
+
+remove_old_version() {
+    echo "[INFO] Removing old version of eql"
+    sudo rm -f /usr/local/bin/eql
+    echo "[INFO] Old version removed "
+}
+
+final_message() {
+    echo "---------------------- Installation complete ----------------------"
+    echo ">>> Run 'eql --help' to get started"
+}
+
+main() {
+    get_latest_release_tag
+    initial_message
+    remove_old_version
+    detect_os
+    download_asset
+    extract_asset
+    move_to_bin
+    cleanup
+    final_message
+}
