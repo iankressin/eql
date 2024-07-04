c895051434c41ac8eddd4e7f72c9776593c4df27
Ian Guimaraes <ianguimaraes31@gmail.com>
2024-07-04 19:32:24 -0300

    refactor: re-work update script to make use of plain binary release

--- eqlup/eqlup.sh
+++ eqlup/eqlup.sh
@@ -5,8 +5,8 @@ REPO_NAME="eql"
 REPO_URL="https://github.com/{$USERNAME}/${REPO_NAME}"
 REPO_API_URL="https://api.github.com/repos/${USERNAME}/${REPO_NAME}"
 
-LINUX_ASSET="x86_64-unknown-linux-musl.zip"
-MAC_ASSET="x86_64-apple-darwin.zip"
+LINUX_ASSET="eql"
+MAC_ASSET="eql"
 
 get_latest_release_tag() {
     LATEST_RELEASE_TAG=$(curl -s "${REPO_API_URL}/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
@@ -46,24 +46,13 @@ detect_os() {
 
 download_asset() {
     echo "[INFO] Downloading asset"
-    curl -L -o latest.zip "${REPO_URL}/releases/download/${LATEST_RELEASE_TAG}/${ASSET_NAME}"
+    curl -L -o eql "${REPO_URL}/releases/download/${LATEST_RELEASE_TAG}/${ASSET_NAME}"
     echo "[INFO] Asset downloaded"
 }
 
-extract_asset() {
-    echo "[INFO] Extracting assets"
-    unzip latest.zip -d latest > /dev/null
-
-    if [ $? -ne 0 ]; then
-        echo "[INFO] Failed to unzip asset"
-        exit 1
-    fi
-    echo "[INFO] Assets extracted"
-}
- 
 move_to_bin() {
     echo "[INFO] Moving to /usr/local/bin"
-    sudo mv latest/eql /usr/local/bin/eql
+    sudo mv eql /usr/local/bin/eql
     chmod +x /usr/local/bin/eql
     echo "[INFO] Installed to /usr/local/bin/eql"
 }
@@ -90,7 +79,6 @@ main() {
     remove_old_version
     detect_os
     download_asset
-    extract_asset
     move_to_bin
     cleanup
     final_message
