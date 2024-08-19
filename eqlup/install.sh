0a16b69b10fe6bc99a2e09fad01425825b7fef60
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-08-19 16:23:59 -0300

    fix: installtion scripts

--- eqlup/install.sh
+++ eqlup/install.sh
@@ -1,6 +1,5 @@
 USERNAME="iankressin"
 REPO_NAME="eql"
-EQL_DIR="$HOME/.eql"
 EQLUP_URL="https://raw.githubusercontent.com/${USERNAME}/${REPO_NAME}/main/eqlup/eqlup.sh"
 
 initial_message() {
@@ -18,7 +17,7 @@ initial_message() {
 
 remove_old_version() {
     echo "[INFO] Removing old version of eqlup"
-    sudo rm -f "$EQL_DIR/eqlup"
+    sudo rm -f /usr/local/bin/eqlup
     echo "[INFO] Old version removed "
 }
 
@@ -27,18 +26,8 @@ download_eqlup() {
     chmod +x eqlup.sh
 }
 
-create_eql_dir_if_needed() {
-    if [ ! -d "$EQL_DIR" ]; then
-        echo "[INFO] EQL directory does not exist. Creating at: $EQL_DIR"
-        mkdir -p "$EQL_DIR"
-        echo "[INFO] Directory created successfully."
-    else
-        echo "[INFO] EQL Directory found. Skipping"
-    fi
-}
-
 move_eqlup() {
-    sudo mv eqlup.sh "$EQL_DIR/eqlup"
+    sudo mv eqlup.sh /usr/local/bin/eqlup
 }
 
 final_message() {
@@ -50,7 +39,6 @@ main() {
     initial_message
     remove_old_version
     download_eqlup
-    create_eql_dir_if_needed
     move_eqlup
     final_message
 }
